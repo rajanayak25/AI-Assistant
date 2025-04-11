85b15227dcbeffb1d86c959dc64d089bf637279d
Rajesh Nayak <95076643+rajanayak25@users.noreply.github.com>
2025-04-11 12:07:10 +0530

    Update profile_data.py
    updated details

--- profile_data.py
+++ profile_data.py
@@ -1,8 +1,8 @@
 profile = {
-    "full_name": "Your Full Name is Rajesh Nayak",
+    "full_name": "Your Full Name is Raj Nayak",
     "address": "You are living in Hyderabad",
-    "mobile_no": "Your Mobile number is 9182232255",
-    "email": "dedavathrajesh@gmail.com",
+    "mobile_no": "Your Mobile number is 123456",
+    "email": "rajnayak@gmail.com",
     "education": "Your Education Qualification is B.Tech Computer Science from KITS, Warangal",
     "profession": "You're working as a Software Engineer in Quadrant Technologies,Hyderabad",
     "hobbies": ["Exploring Technologies", "Writing Notes", "Spending time with family and friends"],
